764ae2bfdc0b06caabe8f765f6cb18f741ffa846
murito7 <mariqn.nz05@gmail.com>
2022-02-16 23:19:20 +0200

    main routes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import HomePage from './pages/HomePage/HomePage';
 function App() {
   return (
     <>
-
+    <Router>
+        <Routes>
+          <Route path="/" element = {<HomePage />} />
+          <Route path="/login" element = {<Login/>}/>
+          <Route path="/register"  element = {<Register/>}/>
+          <Route path="/product/:id" />        
+          <Route path="/products" />    
+        </Routes>
+    </Router>
     </>
   );
 }
