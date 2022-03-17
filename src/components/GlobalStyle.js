4894328cbb1ff5c0ae683a850fd0854806f00323
murito7 <mariqn.nz05@gmail.com>
2022-03-17 13:16:06 +0200

    fixed some colors on github app dark mode

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,23 +1,29 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-    .g-body{
+    .g-body,
+    .g-content-data {
         background: ${({theme}) => theme.backgroundColor} ;
     }
     .g-header .g-title,
     .g-content-header .g-profile-username,
     .g-content-header .g-profile-joindate,
-    .g-content-bio,
-    .g-content-info .g-item h3  {
+    .g-content-info .g-item h3,
+    .g-content-data p  {
         color: ${({theme}) => theme.text} ;
+    }
+    .g-content-data h4, 
+    .g-content-bio,
+    .g-content-header .g-profile-joindate,
+    .g-content-info .g-item h3,
+    .g-search-bar input::placeholder {
+        color: ${({theme}) => theme.lightWhite} ;
     }
     .g-mode-type{
         color: ${({theme}) => theme.icon} ;
     }
     .g-search-bar input[type="text"] {
         background: ${({theme}) => theme.input} ;
-    }
-    .g-search-bar input::placeholder{
         color: ${({theme}) => theme.text} ;
     }
     .g-main {
@@ -30,12 +36,14 @@ export const lightTheme = {
     text: "#fff",
     backgroundColor: "#141D2F",
     icon: "#fff",
-    input: "#1E2A47"
+    input: "#1E2A47",
+    lightWhite: "#b9b9b9"
 };
 
 export const darkTheme = {
     background: "#000",
     text: "#000",
     icon: "#000",
-    input: "#fff"
+    input: "#fff",
+    lightWhite: "#000"
 };
